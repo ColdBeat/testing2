cc440e149200a2e252e7eda68a65e7c83408d345
ColdBeat <34623684+ColdBeat@users.noreply.github.com>
2017-12-17 10:32:34 -0300

    Update stream.js

--- stream.js
+++ stream.js
@@ -1,108 +1,66 @@
-const Long = require('long'),
-	log = require('../logger')
-
-class Readable {
-	constructor(buffer, position = 0) {
-		this.buffer = buffer
-		this.position = position
-	}
-
-	seek(n) { return this.position = n }
-	skip(n) { return this.position += n }
-
-	bool() {
-		const ret = this.byte()
-		if(ret > 1) log.warn(new Error('read byte not 0 or 1 for bool'))
-		return !!ret
-	}
-
-	byte() { return this.buffer.readUInt8(this.position++) }
-
-	bytes(n) { return Buffer.from(this.buffer.slice(this.position, this.position += n)) }
-
-	uint16() {
-		const ret = this.buffer.readUInt16LE(this.position)
-		this.position += 2
-		return ret
-	}
-
-	uint32() {
-		const ret = this.buffer.readUInt32LE(this.position)
-		this.position += 4
-		return ret
-	}
-
-	uint64() {
-		return new Long(this.int32(), this.int32(), true)
-	}
-
-	int16() {
-		const ret = this.buffer.readInt16LE(this.position)
-		this.position += 2
-		return ret
-	}
-
-	int32() {
-		const ret = this.buffer.readInt32LE(this.position)
-		this.position += 4
-		return ret
-	}
-
-	int64() {
-		return new Long(this.int32(), this.int32(), false)
-	}
-
-	float() {
-		const ret = this.buffer.readFloatLE(this.position)
-		this.position += 4
-		return ret
-	}
-
-	string() {
-		for(var ret = '', c; c = this.uint16(); ret += String.fromCharCode(c));
-		return ret
-	}
-}
-
-class Writeable {
-	constructor(length) {
-		this.length = length
-		this.buffer = Buffer.alloc(this.length)
-		this.position = 0
-	}
-
-	seek(n) { this.position = n }
-	skip(n) { this.position += n }
-
-	bool(b) { this.buffer[this.position++] = !!b }
-	byte(n) { this.buffer[this.position++] = n }
-	bytes(buf) {
-		if(buf) {
-			buf.copy(this.buffer, this.position)
-			this.position += buf.length
-		}
-	}
-	uint16(n = 0) { this.position = this.buffer.writeUInt16LE(n & 0xffff, this.position) }
-	uint32(n = 0) { this.position = this.buffer.writeUInt32LE(n >>> 0, this.position) }
-	uint64(obj = {}) {
-		if(typeof obj === 'number') {
-			if(!Number.isSafeInteger(obj)) log.warn(new Error('unsafe integer was provided'))
-			obj = Long.fromNumber(obj, false)
-		}
-		this.uint32(obj.low)
-		this.uint32(obj.high)
-	}
-	float(n = 0) { this.position = this.buffer.writeFloatLE(n, this.position) }
-	string(str = '') { this.buffer.fill(str + '\0', this.position, this.position += (str.length + 1) * 2, 'ucs2') }
-}
-
-Object.assign(Writeable.prototype, {
-	int16: Writeable.prototype.uint16,
-	int32: Writeable.prototype.uint32,
-	int64: Writeable.prototype.uint64
-})
-
-module.exports = {
-	Readable,
-	Writeable
+{
+  "_from": "github:pinkipi/tera-proxy-game",
+  "_id": "tera-proxy-game@1.0.0",
+  "_inBundle": false,
+  "_integrity": "sha1-w8lG8NYtC/g7CwKo0Z5ISErsyJk=",
+  "_location": "/tera-proxy-game",
+  "_phantomChildren": {},
+  "_requested": {
+    "type": "git",
+    "raw": "github:pinkipi/tera-proxy-game",
+    "rawSpec": "github:pinkipi/tera-proxy-game",
+    "saveSpec": "github:pinkipi/tera-proxy-game",
+    "fetchSpec": null,
+    "gitCommittish": "master"
+  },
+  "_requiredBy": [
+    "#USER",
+    "/"
+  ],
+  "_resolved": "github:pinkipi/tera-proxy-game#e10c7247cd3760fea6a6efd5af66cfcede02239e",
+  "_spec": "github:pinkipi/tera-proxy-game",
+  "_where": "Z:\\tera-proxy",
+  "author": {
+    "name": "meishu"
+  },
+  "bugs": {
+    "url": "https://github.com/pinkipi/tera-proxy-game/issues"
+  },
+  "bundleDependencies": false,
+  "dependencies": {
+    "baldera-logger": "^0.1.0",
+    "binary-search": "^1.3.2",
+    "tera-data-parser": "github:pinkipi/tera-data-parser-js"
+  },
+  "deprecated": false,
+  "description": "Modular event handling through a TERA proxy server.",
+  "devDependencies": {
+    "eslint": "^3.19.0",
+    "eslint-config-airbnb-base": "^11.1.3",
+    "eslint-plugin-import": "^2.2.0"
+  },
+  "engines": {
+    "node": ">=6.0.0"
+  },
+  "files": [
+    "lib/"
+  ],
+  "homepage": "https://github.com/pinkipi/tera-proxy-game#readme",
+  "license": "GPL-3.0",
+  "main": "lib/index.js",
+  "name": "tera-proxy-game",
+  "optionalDependencies": {
+    "baldera-logger": "^0.1.0"
+  },
+  "peerDependencies": {
+    "tera-data": "*"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/pinkipi/tera-proxy-game.git"
+  },
+  "scripts": {
+    "lint": "eslint ."
+  },
+  "version": "1.0.0"
 }
